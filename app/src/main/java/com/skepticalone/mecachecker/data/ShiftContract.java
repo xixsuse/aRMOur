8c69f46d2eeb19dbfc227361aed886622b2df3b7
zlodag <zlodag@users.noreply.github.com>
2017-01-29 14:32:03 +1300

    implemented database triggers to prevent shifts overlapping

--- app/src/main/java/com/skepticalone/mecachecker/data/ShiftContract.java
+++ app/src/main/java/com/skepticalone/mecachecker/data/ShiftContract.java
@@ -5,25 +5,64 @@ import android.provider.BaseColumns;
 final class ShiftContract {
     private ShiftContract() {
     }
-
     static class Shift implements BaseColumns {
         static final String
                 TABLE_NAME = "shifts",
                 COLUMN_NAME_START = "start",
                 COLUMN_NAME_END = "end",
-                START_AS_DATE = "strftime('%d/%m/%Y', " + Shift.COLUMN_NAME_START + ", 'unixepoch', 'localtime')",
+                START_AS_DATE = asDate(Shift.COLUMN_NAME_START),
                 START_AS_TIME = asTime(Shift.COLUMN_NAME_START),
                 END_AS_TIME = asTime(Shift.COLUMN_NAME_END),
-                SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + TABLE_NAME + " (" +
+                SQL_CREATE_ENTRIES = "CREATE TABLE " +
+                        TABLE_NAME +
+                        "(" +
                         _ID + " INTEGER PRIMARY KEY," +
-                        COLUMN_NAME_START + " INTEGER NOT NULL," +
-                        COLUMN_NAME_END + " INTEGER NOT NULL)",
-                SQL_DELETE_ENTRIES =
-                        "DROP TABLE IF EXISTS " + TABLE_NAME;
-
+                        COLUMN_NAME_START + " INTEGER NOT NULL UNIQUE," +
+                        COLUMN_NAME_END + " INTEGER NOT NULL UNIQUE," +
+                        "CHECK(" + COLUMN_NAME_START + "<" + COLUMN_NAME_END + ")" +
+                        ")",
+                SQL_CREATE_TRIGGER_BEFORE_INSERT = getTriggerProgram(true),
+                SQL_CREATE_TRIGGER_BEFORE_UPDATE = getTriggerProgram(false),
+                SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        private final static String
+                ERROR_MESSAGE_ON_OVERLAP = "Overlapping shifts",
+                TRIGGER_PROGRAM = "SELECT CASE WHEN (SELECT COUNT(*) FROM " +
+                        TABLE_NAME +
+                        " WHERE (" +
+                        COLUMN_NAME_START +
+                        "<NEW." +
+                        COLUMN_NAME_START +
+                        " AND " +
+                        COLUMN_NAME_END +
+                        ">NEW." +
+                        COLUMN_NAME_START +
+                        ") OR (" +
+                        COLUMN_NAME_START +
+                        "<NEW." +
+                        COLUMN_NAME_END +
+                        " AND " +
+                        COLUMN_NAME_END +
+                        ">NEW." +
+                        COLUMN_NAME_END +
+                        ")) THEN RAISE(ABORT, '" +
+                        ERROR_MESSAGE_ON_OVERLAP +
+                        "') " +
+                        "END";
+        private static String getTriggerProgram(boolean insert) {
+            return "CREATE TRIGGER " +
+                    (insert ? "insert" : "update") +
+                    "_trigger BEFORE " +
+                    (insert ? "INSERT" : "UPDATE") +
+                    " ON " + TABLE_NAME + " BEGIN " + TRIGGER_PROGRAM + ";END";
+        }
         private static String asTime(String columnName) {
-            return "strftime('%H:%M', " + columnName + ", 'unixepoch', 'localtime')";
+            return formatTime(columnName, "%H:%M");
+        }
+        private static String asDate(String columnName) {
+            return formatTime(columnName, "%d/%m/%Y");
+        }
+        private static String formatTime(String columnName, String formatString) {
+            return "strftime('" + formatString + "'," + columnName + ",'unixepoch','localtime')";
         }
     }
 }
