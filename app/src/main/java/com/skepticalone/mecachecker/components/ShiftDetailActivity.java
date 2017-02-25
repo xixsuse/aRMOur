93b53237946477e15ad56b5d558a35a8135722db
zlodag <zlodag@users.noreply.github.com>
2017-02-25 16:45:50 +1300

    Scrolling app bar

--- app/src/main/java/com/skepticalone/mecachecker/components/ShiftDetailActivity.java
+++ app/src/main/java/com/skepticalone/mecachecker/components/ShiftDetailActivity.java
@@ -2,16 +2,24 @@ package com.skepticalone.mecachecker.components;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.skepticalone.mecachecker.R;
 
 public class ShiftDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.shift_detail_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if (savedInstanceState == null) {
             getFragmentManager()
                     .beginTransaction()
-                    .add(android.R.id.content, ShiftDetailFragment.create(getIntent().getLongExtra(ShiftDetailFragment.SHIFT_ID, ShiftDetailFragment.NO_ID)))
+                    .add(R.id.scroll_view, ShiftDetailFragment.create(getIntent().getLongExtra(ShiftDetailFragment.SHIFT_ID, ShiftDetailFragment.NO_ID)))
                     .commit();
         }
     }
