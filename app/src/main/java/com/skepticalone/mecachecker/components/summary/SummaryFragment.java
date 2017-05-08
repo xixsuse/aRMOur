ebfc8dda7e33188b8e5a88e1c508783bf6222825
zlodag <zlodag@users.noreply.github.com>
2017-05-09 01:09:14 +1200

    Improved summary view

--- app/src/main/java/com/skepticalone/mecachecker/components/summary/SummaryFragment.java
+++ app/src/main/java/com/skepticalone/mecachecker/components/summary/SummaryFragment.java
@@ -7,16 +7,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.skepticalone.mecachecker.R;
 import com.skepticalone.mecachecker.data.ShiftContract;
 import com.skepticalone.mecachecker.data.ShiftProvider;
-import com.skepticalone.mecachecker.util.DateTimeUtils;
 
 import org.joda.time.DateTimeConstants;
 import org.joda.time.Duration;
@@ -25,14 +24,53 @@ import java.util.Locale;
 
 public class SummaryFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private final Adapter mAdapter = new AdditionalShiftsAdapter();
+    private static final String[] PROJECTION = {
+            ShiftContract.AdditionalShifts.COLUMN_NAME_START,
+            ShiftContract.AdditionalShifts.COLUMN_NAME_END,
+            ShiftContract.AdditionalShifts.COLUMN_NAME_RATE,
+            ShiftContract.AdditionalShifts.COLUMN_NAME_CLAIMED,
+            ShiftContract.AdditionalShifts.COLUMN_NAME_PAID,
+    };
+    private static final int
+            COLUMN_INDEX_START = 0,
+            COLUMN_INDEX_END = 1,
+            COLUMN_INDEX_RATE = 2,
+            COLUMN_INDEX_CLAIMED = 3,
+            COLUMN_INDEX_PAID = 4;
+
+    private TextView
+            shiftsUnclaimed, shiftsClaimed, shiftsPaid, shiftsTotal,
+            hoursUnclaimed, hoursClaimed, hoursPaid, hoursTotal,
+            moneyUnclaimed, moneyClaimed, moneyPaid, moneyTotal;
+    private ProgressBar
+            shiftsProgress, hoursProgress, moneyProgress;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.summary_fragment, container, false);
-        recyclerView.setAdapter(mAdapter);
-        return recyclerView;
+        View layout = inflater.inflate(R.layout.summary_list_fragment, container, false);
+        View shiftsLayout = layout.findViewById(R.id.shifts);
+        ((TextView) shiftsLayout.findViewById(R.id.title)).setText(R.string.shifts);
+        shiftsUnclaimed = (TextView) shiftsLayout.findViewById(R.id.unclaimed);
+        shiftsClaimed = (TextView) shiftsLayout.findViewById(R.id.claimed);
+        shiftsPaid = (TextView) shiftsLayout.findViewById(R.id.paid);
+        shiftsTotal = (TextView) shiftsLayout.findViewById(R.id.total);
+        shiftsProgress = (ProgressBar) shiftsLayout.findViewById(R.id.progress);
+        View hoursLayout = layout.findViewById(R.id.hours);
+        ((TextView) hoursLayout.findViewById(R.id.title)).setText(R.string.hours);
+        hoursUnclaimed = (TextView) hoursLayout.findViewById(R.id.unclaimed);
+        hoursClaimed = (TextView) hoursLayout.findViewById(R.id.claimed);
+        hoursPaid = (TextView) hoursLayout.findViewById(R.id.paid);
+        hoursTotal = (TextView) hoursLayout.findViewById(R.id.total);
+        hoursProgress = (ProgressBar) hoursLayout.findViewById(R.id.progress);
+        View moneyLayout = layout.findViewById(R.id.money);
+        ((TextView) moneyLayout.findViewById(R.id.title)).setText(R.string.money);
+        moneyUnclaimed = (TextView) moneyLayout.findViewById(R.id.unclaimed);
+        moneyClaimed = (TextView) moneyLayout.findViewById(R.id.claimed);
+        moneyPaid = (TextView) moneyLayout.findViewById(R.id.paid);
+        moneyTotal = (TextView) moneyLayout.findViewById(R.id.total);
+        moneyProgress = (ProgressBar) moneyLayout.findViewById(R.id.progress);
+        return layout;
     }
 
     @Override
@@ -47,7 +85,7 @@ public class SummaryFragment extends Fragment implements LoaderManager.LoaderCal
 //            case SummaryActivity.LOADER_ID_ROSTERED:
 //                break;
             case SummaryActivity.LOADER_ID_ADDITIONAL:
-                return new CursorLoader(getActivity(), ShiftProvider.additionalShiftsUri, AdditionalShiftsAdapter.PROJECTION, null, null, ShiftContract.AdditionalShifts.COLUMN_NAME_START);
+                return new CursorLoader(getActivity(), ShiftProvider.additionalShiftsUri, PROJECTION, null, null, ShiftContract.AdditionalShifts.COLUMN_NAME_START);
 //            case SummaryActivity.LOADER_ID_CROSS_COVER:
 //                break;
             default:
@@ -56,96 +94,65 @@ public class SummaryFragment extends Fragment implements LoaderManager.LoaderCal
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mAdapter.swapCursor(data);
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        int
+                shiftsUnclaimedCount = 0, shiftsClaimedCount = 0, shiftsPaidCount = 0,
+                moneyUnclaimedCount = 0, moneyClaimedCount = 0, moneyPaidCount = 0;
+        Duration
+                durationUnclaimed = Duration.ZERO, durationClaimed = Duration.ZERO, durationPaid = Duration.ZERO;
+
+        while (cursor.moveToNext()) {
+            Duration durationCurrent = new Duration(cursor.getLong(COLUMN_INDEX_START), cursor.getLong(COLUMN_INDEX_END));
+            int moneyCurrent = Math.round(cursor.getInt(COLUMN_INDEX_RATE) * (float) durationCurrent.getMillis() / DateTimeConstants.MILLIS_PER_HOUR);
+            if (!cursor.isNull(COLUMN_INDEX_PAID)) {
+                shiftsPaidCount++;
+                durationPaid = durationPaid.plus(durationCurrent);
+                moneyPaidCount += moneyCurrent;
+            } else if (!cursor.isNull(COLUMN_INDEX_CLAIMED)) {
+                shiftsClaimedCount++;
+                durationClaimed = durationClaimed.plus(durationCurrent);
+                moneyClaimedCount += moneyCurrent;
+            } else {
+                shiftsUnclaimedCount++;
+                durationUnclaimed = durationUnclaimed.plus(durationCurrent);
+                moneyUnclaimedCount += moneyCurrent;
+            }
+        }
+
+        int shiftsTotalCount = shiftsUnclaimedCount + shiftsClaimedCount + shiftsPaidCount;
+        shiftsTotal.setText(String.format(Locale.US, "%d", shiftsTotalCount));
+        if (shiftsTotalCount != 0) {
+            shiftsProgress.setMax(shiftsTotalCount);
+            shiftsProgress.setProgress(shiftsPaidCount);
+            shiftsUnclaimed.setText(String.format(Locale.US, "Unclaimed: %d (%d%%)", shiftsUnclaimedCount, 100 * shiftsUnclaimedCount / shiftsTotalCount));
+            shiftsClaimed.setText(String.format(Locale.US, "Claimed: %d (%d%%)", shiftsClaimedCount, 100 * shiftsClaimedCount / shiftsTotalCount));
+            shiftsPaid.setText(String.format(Locale.US, "Paid: %d (%d%%)", shiftsPaidCount, 100 * shiftsPaidCount / shiftsTotalCount));
+        }
+
+        Duration durationTotal = durationUnclaimed.plus(durationClaimed).plus(durationPaid);
+        hoursTotal.setText(String.format(Locale.US, "%.1f", (float) durationTotal.getStandardMinutes() / DateTimeConstants.MINUTES_PER_HOUR));
+        if (!durationTotal.isEqual(Duration.ZERO)) {
+            hoursProgress.setMax(100);
+            hoursProgress.setProgress((int) (100 * durationPaid.getMillis() / durationTotal.getMillis()));
+            hoursUnclaimed.setText(String.format(Locale.US, "Unclaimed: %.1f (%d%%)", (float) durationUnclaimed.getStandardMinutes() / DateTimeConstants.MINUTES_PER_HOUR, 100 * durationUnclaimed.getMillis() / durationTotal.getMillis()));
+            hoursClaimed.setText(String.format(Locale.US, "Claimed: %.1f (%d%%)", (float) durationClaimed.getStandardMinutes() / DateTimeConstants.MINUTES_PER_HOUR, 100 * durationClaimed.getMillis() / durationTotal.getMillis()));
+            hoursPaid.setText(String.format(Locale.US, "Paid: %.1f (%d%%)", (float) durationPaid.getStandardMinutes() / DateTimeConstants.MINUTES_PER_HOUR, 100 * durationPaid.getMillis() / durationTotal.getMillis()));
+        }
+
+        int moneyTotalCount = moneyUnclaimedCount + moneyClaimedCount + moneyPaidCount;
+        moneyTotal.setText(String.format(Locale.US, "$%.2f", moneyTotalCount / 100f));
+        if (moneyTotalCount != 0) {
+            moneyProgress.setMax(moneyTotalCount);
+            moneyProgress.setProgress(moneyPaidCount);
+            moneyUnclaimed.setText(String.format(Locale.US, "Unclaimed: $%.2f (%d%%)", moneyUnclaimedCount / 100f, 100 * moneyUnclaimedCount / moneyTotalCount));
+            moneyClaimed.setText(String.format(Locale.US, "Claimed: $%.2f (%d%%)", moneyClaimedCount / 100f, 100 * moneyClaimedCount / moneyTotalCount));
+            moneyPaid.setText(String.format(Locale.US, "Paid: $%.2f (%d%%)", moneyPaidCount / 100f, 100 * moneyPaidCount / moneyTotalCount));
+        }
+
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        mAdapter.swapCursor(null);
     }
 
-    private static abstract class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-
-        @Nullable
-        Cursor mCursor = null;
-
-        Adapter() {
-            super();
-        }
-
-        @Override
-        public final ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.summary_list_item, parent, false));
-        }
-
-        void swapCursor(@Nullable Cursor cursor) {
-            mCursor = cursor;
-            notifyDataSetChanged();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            private final TextView primaryText, secondaryText;
-
-            ViewHolder(View itemView) {
-                super(itemView);
-                primaryText = (TextView) itemView.findViewById(R.id.primary_text);
-                secondaryText = (TextView) itemView.findViewById(R.id.secondary_text);
-            }
-        }
-    }
-
-    private static class AdditionalShiftsAdapter extends Adapter {
-
-        static final String[] PROJECTION = {
-                ShiftContract.AdditionalShifts.COLUMN_NAME_START,
-                ShiftContract.AdditionalShifts.COLUMN_NAME_END,
-                ShiftContract.AdditionalShifts.COLUMN_NAME_RATE,
-                ShiftContract.AdditionalShifts.COLUMN_NAME_CLAIMED,
-                ShiftContract.AdditionalShifts.COLUMN_NAME_PAID,
-        };
-        private static final int
-                COLUMN_INDEX_START = 0,
-                COLUMN_INDEX_END = 1,
-                COLUMN_INDEX_RATE = 2,
-                COLUMN_INDEX_CLAIMED = 3,
-                COLUMN_INDEX_PAID = 4;
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            switch (position) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    holder.primaryText.setText(position == 0 ? "All" : position == 1 ? "Unclaimed" : position == 2 ? "Claimed" : "Paid");
-                    int count = 0;
-                    Duration totalDuration = Duration.ZERO;
-                    int totalPaymentInCents = 0;
-                    //noinspection ConstantConditions
-                    if (mCursor.moveToFirst()) {
-                        do {
-                            if (
-                                    (position == 1 && !mCursor.isNull(COLUMN_INDEX_CLAIMED)) ||
-                                            (position == 2 && (mCursor.isNull(COLUMN_INDEX_CLAIMED) || !mCursor.isNull(COLUMN_INDEX_PAID))) ||
-                                            (position == 3 && (mCursor.isNull(COLUMN_INDEX_CLAIMED) || mCursor.isNull(COLUMN_INDEX_PAID)))
-                                    ) continue;
-                            Duration current = new Duration(mCursor.getLong(COLUMN_INDEX_START), mCursor.getLong(COLUMN_INDEX_END));
-                            totalDuration = totalDuration.plus(current);
-                            totalPaymentInCents += Math.round(mCursor.getInt(COLUMN_INDEX_RATE) * (float) current.getMillis() / DateTimeConstants.MILLIS_PER_HOUR);
-                            count++;
-                        } while (mCursor.moveToNext());
-                    }
-                    holder.secondaryText.setText(String.format(Locale.US, "%d shifts (%s) $%.2f", count, DateTimeUtils.getPeriodString(totalDuration.toPeriod()), totalPaymentInCents / 100f));
-                    break;
-                default:
-                    throw new IllegalArgumentException();
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            return mCursor == null ? 0 : 4;
-        }
-    }
 }
