bf416ef7415508b854655629eb277312f54fc4f1
zlodag <zlodag@users.noreply.github.com>
2017-07-09 11:32:15 +1200

    Database working, detecting updates

--- app/src/main/java/com/skepticalone/mecachecker/db/AppDatabase.java
+++ app/src/main/java/com/skepticalone/mecachecker/db/AppDatabase.java
@@ -14,58 +14,22 @@ import com.skepticalone.mecachecker.db.dao.ExpenseDao;
 import com.skepticalone.mecachecker.db.entity.CrossCoverEntity;
 import com.skepticalone.mecachecker.db.entity.ExpenseEntity;
 
-@Database(entities = {CrossCoverEntity.class, ExpenseEntity.class}, version = 2)
+@Database(entities = {CrossCoverEntity.class, ExpenseEntity.class}, version = 4)
 @TypeConverters({LocalDateConverter.class, DateTimeConverter.class, MoneyConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
 
-    private static final String DATABASE_NAME = "newDatabase";
+    private static final String DATABASE_NAME = "database";
+
     private static AppDatabase DATABASE;
 
     public static AppDatabase getInstance(Context applicationContext) {
         if (DATABASE == null) {
-//            applicationContext.deleteDatabase(DATABASE_NAME);
             DATABASE = Room
                     .databaseBuilder(applicationContext, AppDatabase.class, DATABASE_NAME)
                     // TODO: 21/06/17 remove this
                     .allowMainThreadQueries()
-                    .openHelperFactory(new MyFactory())
-//                    .openHelperFactory(new FrameworkSQLiteOpenHelperFactory(){
-//                        @Override
-//                        public SupportSQLiteOpenHelper create(final SupportSQLiteOpenHelper.Configuration configuration) {
-//                            return super.create(SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
-//                                    .callback(new SupportSQLiteOpenHelper.Callback() {
-//                                        @Override
-//                                        public void onConfigure(SupportSQLiteDatabase db) {
-//                                            configuration.callback.onConfigure(db);
-//                                        }
-//
-//                                        @Override
-//                                        public void onOpen(SupportSQLiteDatabase db) {
-//                                            configuration.callback.onOpen(db);
-//                                        }
-//
-//                                        @Override
-//                                        public void onCreate(SupportSQLiteDatabase db) {
-//                                            db.execSQL(CrossCoverEntity.SQL_CREATE_TABLE);
-//                                            db.execSQL(ExpenseEntity.SQL_CREATE_TABLE);
-//                                        }
-//
-//                                        @Override
-//                                        public void onUpgrade(SupportSQLiteDatabase db, int oldVersion, int newVersion) {
-//                                            db.execSQL(CrossCoverEntity.SQL_DROP_TABLE);
-//                                            db.execSQL(ExpenseEntity.SQL_DROP_TABLE);
-//                                            onCreate(db);
-//                                        }
-//                                    })
-//                                    .errorHandler(configuration.errorHandler)
-//                                    .name(configuration.name)
-//                                    .version(configuration.version)
-//                                    .build()
-//                            );
-//                        }
-//                    })
+                    .openHelperFactory(new HelperFactory())
                     .build();
-//            DatabaseInitUtil.initializeDb(DATABASE);
         }
         return DATABASE;
     }
---
+++ app/src/main/java/com/skepticalone/mecachecker/db/HelperFactory.java
@@ -0,0 +1,63 @@
+package com.skepticalone.mecachecker.db;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory;
+
+class HelperFactory extends FrameworkSQLiteOpenHelperFactory {
+
+    @Override
+    public SupportSQLiteOpenHelper create(SupportSQLiteOpenHelper.Configuration configuration) {
+        return super.create(SupportSQLiteOpenHelper.Configuration
+                .builder(configuration.context)
+                .name(configuration.name)
+                .version(configuration.version)
+                .callback(new Callback(configuration.callback))
+                .errorHandler(configuration.errorHandler)
+                .build());
+    }
+
+    private static class Callback extends SupportSQLiteOpenHelper.Callback {
+
+        private final SupportSQLiteOpenHelper.Callback mFrameworkCallback;
+
+        private Callback(SupportSQLiteOpenHelper.Callback frameworkCallback) {
+            mFrameworkCallback = frameworkCallback;
+        }
+
+        @Override
+        public void onConfigure(SupportSQLiteDatabase db) {
+            mFrameworkCallback.onConfigure(db);
+        }
+
+        @Override
+        public void onCreate(SupportSQLiteDatabase db) {
+            mFrameworkCallback.onCreate(db);
+            resetTables(db);
+        }
+
+        @Override
+        public void onUpgrade(SupportSQLiteDatabase db, int oldVersion, int newVersion) {
+            mFrameworkCallback.onUpgrade(db, oldVersion, newVersion);
+            resetTables(db);
+        }
+
+        @Override
+        public void onDowngrade(SupportSQLiteDatabase db, int oldVersion, int newVersion) {
+            mFrameworkCallback.onDowngrade(db, oldVersion, newVersion);
+        }
+
+        @Override
+        public void onOpen(SupportSQLiteDatabase db) {
+            mFrameworkCallback.onOpen(db);
+        }
+
+        private void resetTables(SupportSQLiteDatabase db) {
+            db.execSQL(Contract.CrossCoverShifts.SQL_DROP_TABLE);
+            db.execSQL(Contract.Expenses.SQL_DROP_TABLE);
+            db.execSQL(Contract.CrossCoverShifts.SQL_CREATE_TABLE);
+            db.execSQL(Contract.Expenses.SQL_CREATE_TABLE);
+        }
+
+    }
+}
