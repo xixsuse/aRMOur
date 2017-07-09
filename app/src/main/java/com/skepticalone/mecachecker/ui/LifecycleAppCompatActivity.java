311f8bfe09c2bc20e953f8d1dcd7245f9cebcc6c
zlodag <zlodag@users.noreply.github.com>
2017-07-09 15:13:24 +1200

    Added bottom navigation

--- app/src/main/java/com/skepticalone/mecachecker/ui/LifecycleAppCompatActivity.java
+++ app/src/main/java/com/skepticalone/mecachecker/ui/LifecycleAppCompatActivity.java
@@ -1,39 +1,39 @@
-package com.skepticalone.mecachecker.ui;
-
-import android.arch.lifecycle.LifecycleRegistry;
-import android.arch.lifecycle.LifecycleRegistryOwner;
-import android.os.Bundle;
-import android.support.annotation.CallSuper;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import com.skepticalone.mecachecker.R;
-
-abstract class LifecycleAppCompatActivity extends AppCompatActivity implements LifecycleRegistryOwner {
-
-    private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
-
-    @Override
-    public final LifecycleRegistry getLifecycle() {
-        return lifecycleRegistry;
-    }
-
-    @Override
-    @CallSuper
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(getContentViewWithToolbar());
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //noinspection ConstantConditions
-        getSupportActionBar().setDisplayHomeAsUpEnabled(getDisplayHomeAsUpEnabled());
-    }
-
-    @LayoutRes
-    abstract int getContentViewWithToolbar();
-
-    abstract boolean getDisplayHomeAsUpEnabled();
-
-}
+//package com.skepticalone.mecachecker.ui;
+//
+//import android.arch.lifecycle.LifecycleRegistry;
+//import android.arch.lifecycle.LifecycleRegistryOwner;
+//import android.os.Bundle;
+//import android.support.annotation.CallSuper;
+//import android.support.annotation.LayoutRes;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//
+//import com.skepticalone.mecachecker.R;
+//
+//abstract class LifecycleAppCompatActivity extends AppCompatActivity implements LifecycleRegistryOwner {
+//
+//    private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
+//
+//    @Override
+//    public final LifecycleRegistry getLifecycle() {
+//        return lifecycleRegistry;
+//    }
+//
+//    @Override
+//    @CallSuper
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(getContentViewWithToolbar());
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        //noinspection ConstantConditions
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(getDisplayHomeAsUpEnabled());
+//    }
+//
+//    @LayoutRes
+//    abstract int getContentViewWithToolbar();
+//
+//    abstract boolean getDisplayHomeAsUpEnabled();
+//
+//}
--- app/src/main/java/com/skepticalone/mecachecker/ui/MainActivity.java
+++ app/src/main/java/com/skepticalone/mecachecker/ui/MainActivity.java
@@ -1,20 +1,23 @@
 package com.skepticalone.mecachecker.ui;
 
+import android.arch.lifecycle.LifecycleActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
+import android.view.MenuItem;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 import com.skepticalone.mecachecker.R;
 
-public class MainActivity extends LifecycleAppCompatActivity implements ListFragment.Callbacks {
+public class MainActivity extends LifecycleActivity implements BottomNavigationView.OnNavigationItemSelectedListener, ListFragment.Callbacks {
 
     private static final String LIST_FRAGMENT = "LIST_FRAGMENT", DETAIL_FRAGMENT = "DETAIL_FRAGMENT";
-    private static final String MASTER_TO_DETAIL = "MASTER_TO_DETAIL";
-    private static final String TAG = "MainActivity";
+    //    private static final String MASTER_TO_DETAIL = "MASTER_TO_DETAIL";
+//    private static final String TAG = "MainActivity";
     private FloatingActionMenu mFabMenu;
     private FloatingActionButton mFabNormalDay, mFabLongDay, mFabNightShift;
     private boolean mTwoPane;
@@ -22,33 +25,75 @@ public class MainActivity extends LifecycleAppCompatActivity implements ListFrag
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
         mTwoPane = findViewById(R.id.detail_fragment_container) != null;
-        Log.i(TAG, "onCreate: mTwoPane = " + mTwoPane);
+//        Log.i(TAG, "onCreate: mTwoPane = " + mTwoPane);
         mFabMenu = findViewById(R.id.fab_menu);
         mFabNormalDay = mFabMenu.findViewById(R.id.fab_normal_day);
         mFabLongDay = mFabMenu.findViewById(R.id.fab_long_day);
         mFabNightShift = mFabMenu.findViewById(R.id.fab_night_shift);
         mFabMenu.close(false);
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction =
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .add(R.id.list_fragment_container, new ExpenseListFragment(), LIST_FRAGMENT);
-            if (mTwoPane) {
-                transaction.add(R.id.detail_fragment_container, new ExpenseDetailFragment(), DETAIL_FRAGMENT);
-            }
-            transaction.commit();
+//        if (savedInstanceState == null) {
+//            FragmentTransaction transaction =
+//                    getSupportFragmentManager()
+//                            .beginTransaction()
+//                            .add(R.id.list_fragment_container, new ExpenseListFragment(), LIST_FRAGMENT);
+//            if (mTwoPane) {
+//                transaction.add(R.id.detail_fragment_container, new ExpenseDetailFragment(), DETAIL_FRAGMENT);
+//            }
+//            transaction.commit();
+//        }
+    }
+//
+//    @Override
+//    boolean getDisplayHomeAsUpEnabled() {
+//        return false;
+//    }
+//
+//    @Override
+//    int getContentViewWithToolbar() {
+//        return R.layout.main_activity;
+//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        ListFragment listFragment;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                listFragment = new ExpenseListFragment();
+                break;
+            case R.id.navigation_dashboard:
+                listFragment = new CrossCoverListFragment();
+                break;
+            case R.id.navigation_notifications:
+                listFragment = new ExpenseListFragment();
+                break;
+            default:
+                return false;
         }
-    }
-
-    @Override
-    boolean getDisplayHomeAsUpEnabled() {
-        return false;
-    }
-
-    @Override
-    int getContentViewWithToolbar() {
-        return R.layout.main_activity;
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.list_fragment_container, listFragment, LIST_FRAGMENT);
+        if (mTwoPane) {
+            DetailFragment detailFragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    detailFragment = new ExpenseDetailFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    detailFragment = new CrossCoverDetailFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    detailFragment = new ExpenseDetailFragment();
+                    break;
+                default:
+                    return false;
+            }
+            transaction.replace(R.id.detail_fragment_container, detailFragment, DETAIL_FRAGMENT);
+        }
+        transaction.commit();
+        return true;
     }
 
     @Override
