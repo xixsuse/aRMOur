2222adff78fbf01dafb8e6dbdc32dc95d639d8a7
zlodag <11688686+zlodag@users.noreply.github.com>
2017-11-19 18:06:06 +1300

    Improved help

--- app/src/main/java/com/skepticalone/armour/adapter/RosteredShiftHelpAdapter.java
+++ app/src/main/java/com/skepticalone/armour/adapter/RosteredShiftHelpAdapter.java
@@ -8,9 +8,13 @@ import com.skepticalone.armour.R;
 final class RosteredShiftHelpAdapter extends HelpAdapter {
 
     private final static HelpViewHolder.Binder[] ROWS = {
-            FIRST_HELP_ITEM,
-            new HelpViewHolder.Binder(true, R.drawable.ic_normal_day_white_24dp, "Add a normal day", "Another help item, which has a floating action button as an icon and an even longer description"),
+            new HelpViewHolder.Binder(true, R.drawable.ic_normal_day_white_24dp, "Add a normal day", null),
             new HelpViewHolder.Binder(true, R.drawable.ic_long_day_white_24dp, "Add a long day", null),
+            new HelpViewHolder.Binder(true, R.drawable.ic_night_shift_white_24dp, "Add a night shift", null),
+            new HelpViewHolder.Binder(false, R.drawable.compliant_black_24dp, "Compliant shift", "Passes all compliance checks"),
+            new HelpViewHolder.Binder(false, R.drawable.non_compliant_red_24dp, "Non-compliant shift", "Fails at least one compliance check"),
+            new HelpViewHolder.Binder(false, R.drawable.ic_check_circle_24dp, "Selected shift", "Long click to select shifts"),
+            new HelpViewHolder.Binder(false, R.drawable.run_review_white_24dp, "Non-compliant shift", "Fails at least one compliance check"),
     };
 
     RosteredShiftHelpAdapter(@NonNull Context context) {
--- app/src/main/java/com/skepticalone/armour/help/HelpActivity.java
+++ app/src/main/java/com/skepticalone/armour/help/HelpActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.skepticalone.armour.ui.common.MainActivity;
+import com.skepticalone.armour.R;
 
 public final class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(android.R.id.content, HelpFragment.newInstance(getIntent().getIntExtra(MainActivity.ITEM_TYPE, 0))).commit();
-        }
+        setContentView(R.layout.rostered_help);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().add(android.R.id.content, HelpFragment.newInstance(getIntent().getIntExtra(MainActivity.ITEM_TYPE, 0))).commit();
+//        }
     }
 }
